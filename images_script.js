3c27203d57e31ff164507a27ceda2ea384b31364
ConfusedSoni <arnavsoni1020@gmail.com>
2025-03-01 11:31:28 -0800

    added images

--- images_script.js
+++ images_script.js
@@ -1,3 +1,3 @@
-const imageGallery[
-    
+const imageGallery = [
+    https://thefader-res.cloudinary.com/images/w_1440,c_limit,f_auto,q_auto:eco/tddjxrfipuoumtenntz1/how-to-shoot-tyler-the-creator.jpg
 ]
